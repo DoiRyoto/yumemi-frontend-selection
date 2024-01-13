a4eabb1b5dbd81c375c1a5057f5c80f48938ca59
DoiRyoto <r_doi@vislab.cs.tsukuba.ac.jp>
2024-01-13 22:16:40 +0900

    add: accordionメニューを追加 都道府県ボタンを非表示にできるようにした

--- src/app/components/templates/top/index.tsx
+++ src/app/components/templates/top/index.tsx
@@ -1,6 +1,7 @@
 import { Suspense } from "react";
 import { PrefPopulationLineChartContainer } from "./container/PrefPopulationLineChartContainer";
 import { PrefToggleListContainer } from "./container/PrefToggleListContainer";
+import { Accordion } from "../../atoms/Accordion";
 
 type Props = {
   pref?: string;
@@ -9,10 +10,11 @@ type Props = {
 export const Top = async ({ pref }: Props) => {
   return (
     <main className="top">
-      <h1 className="top-title">都道府県</h1>
-      <Suspense>
-        <PrefToggleListContainer />
-      </Suspense>
+      <Accordion label="都道府県" defaultIsOpen={true}>
+        <Suspense>
+          <PrefToggleListContainer />
+        </Suspense>
+      </Accordion>
       <Suspense>
         <PrefPopulationLineChartContainer pref={pref} />
       </Suspense>
