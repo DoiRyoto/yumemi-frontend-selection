ef5d8b332d067ec0d35b40f573b925b2495bfebf
DoiRyoto <r_doi@vislab.cs.tsukuba.ac.jp>
2024-01-15 11:38:48 +0900

    chore: 不要な変数の削除

--- src/app/components/templates/top/container/PrefPopulationLineChartContainer/index.tsx
+++ src/app/components/templates/top/container/PrefPopulationLineChartContainer/index.tsx
@@ -1,18 +1,13 @@
 import React from "react";
 import { PrefPopulationLineChart } from "../../PrefPopulationLineChart";
-import { getHighChartsOptions } from "../../PrefPopulationLineChart/utils";
 import { fetchPrefectureNames } from "@/services/prefectures";
 import { fetchPopulationsByPrefCodes } from "@/services/population";
 
 type Props = {
   pref?: string;
-  populationType?: string;
 };
 
-export const PrefPopulationLineChartContainer = async ({
-  pref,
-  populationType,
-}: Props) => {
+export const PrefPopulationLineChartContainer = async ({ pref }: Props) => {
   const rowPrefData = await fetchPrefectureNames();
   const allPrefDataList = rowPrefData.result;
 
